1df169ce2b6c208850672cbb579f2f88494f080f
Erlend Hamnaberg <erlend@hamnaberg.net>
2018-10-11 19:13:08 +0200

    More combinators
    Kotlin style destructors
    mapN to copy values

--- project/Tuples.scala
+++ project/Tuples.scala
@@ -14,7 +14,12 @@ object Tuples {
       def _2 =
         s"""  public Tuple2<A2, A1> swap(){
            |    return new Tuple2<>(_2, _1);
+           |  }
+           |
+           |  public java.util.Map.Entry<A1, A2> asEntry() {
+           |    return new java.util.AbstractMap.SimpleImmutableEntry<>(_1, _2);
            |  }""".stripMargin
+
 
       val content =
         s"""package $pkg;
@@ -22,6 +27,8 @@ object Tuples {
            |import java.io.Serializable;
            |import java.util.Objects;
            |import java.util.function.Function;
+           |import java.util.List;
+           |import java.util.Arrays;
            |
            |public final class Tuple$n<$types> implements Serializable {
            |  ${params.map { case (a, i) => s"public final $a $i;" }.mkString("\n  ")}
@@ -32,13 +39,17 @@ object Tuples {
            |
            |${if (n == 2) _2 else ""}
            |
-           |  public static <$types, B> $curried curry(Function<Tuple$n<$types>, B> f){
+           |  public static <$types, B> $curried curry(Function<Tuple$n<$types>, B> f) {
            |    return ${names.mkString(" -> ")} -> f.apply(new Tuple$n<>(${names.mkString(", ")}));
            |  }
            |
-           |  public static <$types, B> Function<Tuple$n<$types>, B> uncurry($curried f){
+           |  public static <$types, B> Function<Tuple$n<$types>, B> uncurry($curried f) {
            |    return t -> f${names.map(a => s".apply(t.$a)").mkString("")};
            |  }
+           |
+           |${params.zipWithIndex.map{case ((a, field), idx) => s"  public $a component${idx + 1}() {\n    return $field;\n  }\n"}.mkString("\n")}
+           |
+           |${params.zipWithIndex.map{case ((a, field), idx) => s"  public <B> Tuple$n<${params.map(_._1).updated(idx, "B").mkString(", ")}> map${idx + 1}(Function<? super $a, ? extends B> f) {\n    return new Tuple$n<>(${names.updated(idx, s"f.apply($field)").mkString(", ")});\n  }\n"}.mkString("\n")}
            |
            |  @Override
            |  public int hashCode() {
@@ -47,17 +58,21 @@ object Tuples {
            |
            |  @Override
            |  public boolean equals(Object obj) {
-           |    if(obj == this){
+           |    if (obj == this) {
            |      return true;
-           |    } else if(obj instanceof Tuple$n){
+           |    } else if (obj instanceof Tuple$n) {
            |      Tuple$n other = (Tuple$n)obj;
            |      return ${names.map(n => s"Objects.equals($n, other.$n)").mkString(" && ")};
            |    } else return false;
            |  }
            |
+           |  public List<Object> asList() {
+           |    return Arrays.asList(${names.mkString(", ")});
+           |  }
+           |
            |  @Override
-           |  public String toString(){
-           |    return "(" + ${names.mkString(" + \", \" + ")} + ")";
+           |  public String toString() {
+           |    return asList().stream().map(Object::toString).collect(java.util.stream.Collectors.joining(", ", "(", ")"));
            |  }
            |}
       """.stripMargin
