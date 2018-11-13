6eac5a6f7f55f6abaee508c9ac0495d9d0ba1294
Erlend Hamnaberg <erlend@hamnaberg.net>
2018-11-13 10:43:44 +0100

    Inline some variables in generator

--- project/Functions.scala
+++ project/Functions.scala
@@ -11,8 +11,6 @@ object Functions {
       val types = params.map(_._1).mkString(", ")
       val names = params.map(_._2).mkString(", ")
       val paramsString = params.map{case (tpe, name) => s"$tpe $name"}.mkString(", ")
-      val tupledValues = (1 to n).map(i => s"t._$i").mkString(", ")
-      val curried = params.map(_._2).mkString(" -> ")
       val curriedTypes = params.map{case (tpe, _) => s"Function<$tpe"}.mkString(", ") + ", B" + (">" * n)
 
       val content =
@@ -35,11 +33,11 @@ object Functions {
            |   }
            |
            |   default Function<Tuple$n<$types>, B> tupled() {
-           |      return t -> apply($tupledValues);
+           |      return t -> apply(${(1 to n).map(i => s"t._$i").mkString(", ")});
            |   }
            |
            |   default $curriedTypes curried() {
-           |      return $curried -> apply($names);
+           |      return ${params.map(_._2).mkString(" -> ")} -> apply($names);
            |   }
            |
            |   default <V> Function$n<$types, V> andThen(Function<? super B, ? extends V> after) {
