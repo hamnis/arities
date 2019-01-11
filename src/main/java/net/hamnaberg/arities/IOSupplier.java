e6559cad32f3ab48213cff874359d8b28f0442ff
Erlend Hamnaberg <erlend@hamnaberg.net>
2019-01-11 19:37:08 +0100

    Supplier: To and from checked/unchecked

--- src/main/java/net/hamnaberg/arities/IOSupplier.java
+++ src/main/java/net/hamnaberg/arities/IOSupplier.java
@@ -1,7 +1,22 @@
 package net.hamnaberg.arities;
 
 import java.io.IOException;
+import java.util.function.Supplier;
 
 public interface IOSupplier<A> {
     A get() throws IOException;
+
+    default Supplier<A> unchecked() {
+        return () -> {
+            try {
+                return get();
+            } catch (IOException e) {
+                throw Sneaky.sneakyThrow(e);
+            }
+        };
+    }
+
+    static <A> IOSupplier<A> fromSupplier(Supplier<A> other) {
+        return () -> Sneaky.getOrThrowE(other::get, IOException.class, IOException::new);
+    }
 }
--- src/main/java/net/hamnaberg/arities/SQLSupplier.java
+++ src/main/java/net/hamnaberg/arities/SQLSupplier.java
@@ -1,7 +1,22 @@
 package net.hamnaberg.arities;
 
 import java.sql.SQLException;
+import java.util.function.Supplier;
 
 public interface SQLSupplier<A> {
     A get() throws SQLException;
+
+    default Supplier<A> unchecked() {
+        return () -> {
+            try {
+                return get();
+            } catch (SQLException e) {
+                throw Sneaky.sneakyThrow(e);
+            }
+        };
+    }
+
+    static <A> SQLSupplier<A> fromSupplier(Supplier<A> other) {
+        return () -> Sneaky.getOrThrowE(other::get, SQLException.class, SQLException::new);
+    }
 }
