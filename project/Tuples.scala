14d6876a4a72c0cc5a1480f55622385c34eec96c
Erlend Hamnaberg <erlend@hamnaberg.net>
2024-09-12 12:07:02 +0200

    use field access inside the record

--- project/Tuples.scala
+++ project/Tuples.scala
@@ -25,39 +25,37 @@ object Tuples {
 
       def _2 =
         s"""  public Tuple2<A2, A1> swap(){
-           |    return new Tuple2<>(_2(), _1());
+           |    return new Tuple2<>(_2, _1);
            |  }
            |
            |  public java.util.Map.Entry<A1, A2> asEntry() {
-           |    return new java.util.AbstractMap.SimpleImmutableEntry<>(_1(), _2());
+           |    return new java.util.AbstractMap.SimpleImmutableEntry<>(_1, _2);
            |  }""".stripMargin
 
       val content =
         s"""package $pkg;
            |
-           |import java.io.Serializable;
            |import java.util.Objects;
            |import java.util.function.Function;
            |import java.util.List;
            |import java.util.Arrays;
            |
-           |public record Tuple$n<$types>(${params.map { case (a, i) => s"$a $i" }.mkString(", ")}) implements Serializable {
-           |
+           |public record Tuple$n<$types>(${params.map { case (a, i) => s"$a $i" }.mkString(", ")}) {
            |
            |${if (n < 27) append(n, types, names) else ""}
            |${if (n == 2) _2 else ""}
            |
            |  public static <$types, B> $curried curry(Function<Tuple$n<$types>, B> f) {
-           |    return ${names.mkString(" -> ")} -> f.apply(new Tuple$n<>(${names.map(n => n).mkString(", ")}));
+           |    return ${names.mkString(" -> ")} -> f.apply(new Tuple$n<>(${names.mkString(", ")}));
            |  }
            |
            |  public static <$types, B> Function<Tuple$n<$types>, B> uncurry($curried f) {
-           |    return t -> f${names.map(a => s".apply(t.$a())").mkString("")};
+           |    return t -> f${names.map(a => s".apply(t.$a)").mkString("")};
            |  }
            |
-           |${params.zipWithIndex.map{case ((a, field), idx) => s"  public $a component${idx + 1}() {\n    return $field();\n  }\n"}.mkString("\n")}
+           |${params.zipWithIndex.map{case ((a, field), idx) => s"  public $a component${idx + 1}() {\n    return $field;\n  }\n"}.mkString("\n")}
            |
-           |${params.zipWithIndex.map{case ((a, field), idx) => s"  public <B> Tuple$n<${params.map(_._1).updated(idx, "B").mkString(", ")}> map${idx + 1}(Function<? super $a, ? extends B> f) {\n    return new Tuple$n<>(${names.updated(idx, s"f.apply($field())").mkString(", ")});\n  }\n"}.mkString("\n")}
+           |${params.zipWithIndex.map{case ((a, field), idx) => s"  public <B> Tuple$n<${params.map(_._1).updated(idx, "B").mkString(", ")}> map${idx + 1}(Function<? super $a, ? extends B> f) {\n    return new Tuple$n<>(${names.updated(idx, s"f.apply($field)").mkString(", ")});\n  }\n"}.mkString("\n")}
            |
            |
            |  public int arity() {
@@ -65,7 +63,7 @@ object Tuples {
            |  }
            |
            |  public List<Object> asList() {
-           |    return Arrays.asList(${names.map(n => n + "()").mkString(", ")});
+           |    return Arrays.asList(${names.mkString(", ")});
            |  }
            |
            |  @Override
@@ -73,7 +71,6 @@ object Tuples {
            |    return asList().stream().map(Object::toString).collect(java.util.stream.Collectors.joining(", ", "(", ")"));
            |  }
            |
-           |  private static final long serialVersionUID = ${serialVersionUID}L;
            |}
       """.stripMargin
       IO.write(file, content)
